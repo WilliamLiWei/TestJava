b4b8ebd923dcd5929a376aa634867bffed97a5a4
李伟 <liweimyfild_0@163.com>
2020-04-28 16:49:34 +0800

    添加了静态代理和动态代理

--- testMain.java
+++ testMain.java
@@ -1,3 +1,9 @@
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.HashSet;
+import TestJava.*;
+
 public class testMain {
 
 	public static void main(final String[] args) {
@@ -15,6 +21,38 @@ public class testMain {
 				System.out.print(c);
 			}
 		}
-		System.out.println("\nEnd;");
+        System.out.println("\nEnd;");
+        
+        		// 构造一个hashmap对象
+		HashMap<String, String> map1 = new HashMap<>();
+		
+		// 往hashmap中添加数据，如果key重复，则会覆盖
+		map1.put("1", "白百合");
+		map1.put("1", "马蓉");
+		
+		// 继续添加
+		map1.put("2", "王宝强");
+		map1.put("3", "黄渤");
+		map1.put("4", "靳东");
+		map1.put("5", null);
+		
+		
+		// 获取map中的数据
+		String a = map1.get("2");
+		System.out.println(a);
+
+		// HelloProxy helloProxy = new HelloProxy();
+		// helloProxy.sayHello();
+		
+		// 执行动态代理
+		System.getProperties().setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+
+        HelloInterface hello = new Hello();
+        
+        InvocationHandler handler = new ProxyHandler(hello);
+
+        HelloInterface proxyHello = (HelloInterface) Proxy.newProxyInstance(hello.getClass().getClassLoader(), hello.getClass().getInterfaces(), handler);
+
+        proxyHello.sayHello();
 	}	
 }
